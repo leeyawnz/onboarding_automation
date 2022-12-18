25ff05d4ae0aee688c18bc3095826bac7d60cbbc
leeyawnz <58063567+leeyawnz@users.noreply.github.com>
2022-12-19 02:42:02 +0800

    Update excel_to_json_authorizer.py

--- SFT/excel_to_json_authorizer.py
+++ SFT/excel_to_json_authorizer.py
@@ -1,80 +1,78 @@
+# import statements
 import os
 from openpyxl import Workbook, load_workbook
 from openpyxl.utils import get_column_letter
 
-# input statements
-wb_name = input('Enter Excelsheet Name (.xlsx): ')
-data_tb_len = input('Enter length of data (Last cell + 1): ')
-data_tb_len = int(data_tb_len)
-appname = input('Enter Application Team Name: ')
+# constants
+wb_name = input('Insert excelsheet file name (.xlsx): ')
+env = input('what SFT environment is this?: ')
+appname = input('Insert app team name: ')
 
 # loading workbook
 wb = load_workbook(wb_name)
 ws = wb.active
 
-# full file type list
-full_ft_list = []
-for col in range(1, 2):
-    char = get_column_letter(col)
-    for row in range(1, data_tb_len):
-        cell = ws[char + str(row)].value
-        full_ft_list.append(cell)
+# Functions
+def get_length(a, b):
+    for col in range(a, b):
+        char = get_column_letter(col)
+        i = 1
+        while True:
+            cell = ws[char + str(i)].value
+            if cell == None:
+                return i
+            else:
+                i += 1
+    return i
 
+def get_array(a, b, end_of_row):
+    array = []
+    for col in range(a, b):
+        char = get_column_letter(col)
+        for row in range(1, end_of_row):
+            cell = ws[char + str(row)].value
+            array.append(cell)
+    return array
 
-with open(f'{appname}.txt', 'a') as file:
-    json_file_header = '''
-{
-    "sft-authorizer-sit": [
-'''
-    file.write(json_file_header)
+def sft_template(appname, array, i):
+    sft_entry = f'''
+        {{
+            "PutRequest": {{
+                "Item": {{
+                    "pk": {{
+                        "s": "{appname}#{array[i]}"
+                    }},
+                    "pkstatus": {{
+                        "s": true
+                    }}
+                }}
+            }}
+        }}'''
+    return sft_entry
+
+# SFT JSON script
+sft_array_length = get_length(1, 2)
+sft_array = get_array(1, 2, sft_array_length)
+sft_body = sft_template(appname, sft_array, 1) + ','
+sft_header = f'''{{
+    "sft-authorizer-{env}": ['''
+sft_footer = f'''
+    ]
+}}'''
 
 i = 0
-while i < len(full_ft_list):
-    if i != len(full_ft_list) - 1:
-        with open(f'{appname}.txt', 'a') as file:
-            json_template = f"""
-        {{
-            "PutRequest": {{
-                "Item": {{
-                    "pk": {{
-                        "s": "{appname}#{full_ft_list[i]}"
-                    }},
-                    "pkstatus": {{
-                        "s": true
-                    }}
-                }}
-            }}
-        }},
-"""
-            file.write(json_template)
-            i += 1
+while i < sft_array_length - 1:
+    if i != sft_array_length - 2:
+        sft_header += sft_template(appname, sft_array, i) + ','
+        i += 1
     else:
-        with open(f'{appname}.txt', 'a') as file:
-            json_template = f"""
-        {{
-            "PutRequest": {{
-                "Item": {{
-                    "pk": {{
-                        "s": "{appname}#{full_ft_list[i]}"
-                    }},
-                    "pkstatus": {{
-                        "s": true
-                    }}
-                }}
-            }}
-        }}
-"""
-            file.write(json_template)
-            i += 1
-
+        sft_header += sft_template(appname, sft_array, i)
+        i += 1
+sft_header += sft_footer
 
 with open(f'{appname}.txt', 'a') as file:
-    json_file_footer = '''
-    ]
-}
-'''
-    file.write(json_file_footer)
+    file.write(sft_header)
 
 txt_file = os.path.join('.', f'{appname}.txt')
-yaml_file = txt_file.replace('.txt', '.json')
-os.rename(txt_file, yaml_file)
+json_file = txt_file.replace('.txt', '.json')
+os.rename(txt_file, json_file)
