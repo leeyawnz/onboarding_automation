6037a0ad250ab82b9cf5b99abf4079bcfd9bd432
leeyawnz <58063567+leeyawnz@users.noreply.github.com>
2022-12-24 23:27:02 +0800

    Update excel_to_yaml_config.py

--- MDS/excel_to_yaml_config.py
+++ MDS/excel_to_yaml_config.py
@@ -1,41 +1,73 @@
 import os
 from openpyxl import Workbook, load_workbook
 from openpyxl.utils import get_column_letter
+from datetime import datetime
 
-# input statements
-wb_name = input('Enter Excelsheet Name (.xlsx): ')
-data_tb_len = input('Enter length of data (Last cell + 1): ')
-data_tb_len = int(data_tb_len)
-appname = input('Enter Application Team Name: ')
+# Functions
+def workbookName():
+    path = './excels/'
+    for file in os.listdir(path):
+        if (os.path.isfile(os.path.join(path, file)) and ".xlsx" in file):
+            xlsx_file = path + file
+            return xlsx_file
 
-# loading workbook
-wb = load_workbook(wb_name)
-ws = wb.active
+def getLengthOfArray(a, b):
+    for col in range(a, b):
+        char = get_column_letter(col)
+        i = 1
+        while True:
+            cell = activeWorksheet[char + str(i)].value
+            if cell == None:
+                return i
+            else:
+                i += 1
+    return i
 
-# Query template list
-full_template_list = []
-for col in range(1, 2):
-    char = get_column_letter(col)
-    for row in range(1, data_tb_len):
-        cell = ws[char + str(row)].value
-        full_template_list.append(cell)
+def getArrayItems(a, b, end_of_row):
+    array = []
+    for col in range(a, b):
+        char = get_column_letter(col)
+        for row in range(2, end_of_row):
+            cell = activeWorksheet[char + str(row)].value
+            array.append(cell)
+    return array
 
+# Loading xlsx
+workbookName = workbookName()
+loadingWorkbook = load_workbook(workbookName)
+activeWorksheet = loadingWorkbook.active
+
+# Getting array of items
+appname = workbookName.split("/")[-1]
+appname = appname.split(".")[0]
+length = getLengthOfArray(3, 4)
+entryArray = getArrayItems(3, 4, length)
+
+# MDS query template YAML file
+yamlFile = f'''# {datetime.now()}
+---
+'''
 i = 0
-while i < len(full_template_list):
-    with open(f'{appname}.txt', 'a') as file:
-        yaml_template = f"""
-# {i + 1}.
-- pkstatus: true
-  dataSource: hasura
-  querytemplate: '{full_template_list[i]}'
-  pk:
-  appid: {appname}
-  paramValuesSchema:
-  required:
-"""
-        file.write(yaml_template)
-        i += 1
+while i < len(entryArray):
+    yamlEntry = f"""# {i + 1}
+-   pkstatus: true
+    dataSource: hasura
+    querytemplate: '{entryArray[i]}'
+    pk:
+    appid: {appname}
+    paramValuesSchema:
+    required:
 
-txt_file = os.path.join('.', f'{appname}.txt')
+"""
+    yamlFile += yamlEntry
+    i += 1
+
+# Creating query template YAML file
+with open('./artifacts/mds_querytemplate.txt', 'w') as file:
+    file.write(yamlFile)
+
+txt_file = os.path.join('./artifacts/', 'mds_querytemplate.txt')
 yaml_file = txt_file.replace('.txt', '.yaml')
 os.rename(txt_file, yaml_file)
+
+print('MDS Query Template: File Generation Completed')
